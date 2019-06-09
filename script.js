009986b091c21e848d5741db33de4f8ffd85b67d
gleekzorp <gleek.dan@gmail.com>
2019-06-09 14:29:33 -0600

    Functionality works and added styles

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+function myFunction() {
+    document.body.style.background = randomHex();
+    document.getElementById('hex-text').innerText = randomHex();
+}
+
 function randomHex() {
     var hex = [];
     const combo = ['a', 'b', 'c', 'd', 'e', 'f', 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for (let i = 0; i < 7; i++) {
-      
-      let randomThing = Math.floor(Math.random() * combo.length)
-      
-      hex.push(combo[randomThing])
-      
+    
+    for (let i = 0; i < 6; i++) {
+        
+        let randomThing = Math.floor(Math.random() * combo.length)
+        
+        hex.push(combo[randomThing])
     }
-    return hex.join('')
-}
-randomHex()
+        console.log(`"#${hex.join('')}"`);
+        return `#${hex.join('')}`
+  }
